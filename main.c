9597fd9f867579baeaf1b0951d63b6140fcba150
MahmoudToson <91160037+MahmoudToson@users.noreply.github.com>
2023-11-15 00:52:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,41 +6,19 @@
  * @av: Arguments
  * Return: 0 Always (success)
 */
-int main(int ac, char **argv)
+int main(void) {
+    char *cmd;
+if(isatty(STDIN_FILENO))
 {
-    char *line = NULL, **cmd = NULL;
-    int sts = 0;
-    (void) ac;
-if (isatty(STDIN_FILENO)){
-   while (1)
-    {
-        line = read_line();
-        if (line == NULL) /* Reach EOF CTRL + D*/
-        {
-            write(STDOUT_FILENO, "\n", 1);
-            return (sts);
+ while(1) {
+        printf("M&H-$ ");
+        cmd = read_line();
+
+        if (cmd != NULL) {
+            execute(cmd);
+            free(cmd);
         }
-	    execute();
-    }
-}    
- cmd = tokenizer(line);
-        if (!cmd)
-            continue;
-
-<<<<<<< HEAD
-
-
-        sts = execute_cmd(cmd, argv); /*Execute line*/
-
-=======
-        for (i = 0; cmd
-			[i]; i++)
-        {
-            printf("%s\n", cmd[i]);
-            free(cmd[i]), cmd[i] = NULL;
-        }
-        free(cmd), cmd = NULL;
-
->>>>>>> 340cbdef06cbc9a685a6818105bb146e1ab57506
-    }
+    } 
+}
+    return EXIT_SUCCESS;
 }
